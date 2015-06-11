9776417291cab9aee048332c6f980c41054f25ad
Ian Brennan <ian.brennan@icloud.com>
2015-06-11 10:20:14 -0600

    key decoder working

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+  var keyDecoder = {'87':'w', '65':'a', '83':'s', '68':'d'}
 
 
   $('body').on("keydown", function whichKey(e){
 
     var keyCode = e.keyCode;
     if(keyCode){
-      alert("you hit" + keyCode.toString())
+      console.log("you hit " + keyDecoder[keyCode.toString()])
     }
 
   })
