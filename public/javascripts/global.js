dadee8a0940d042660a6cdf39cc4a8da6c768514
Ian Brennan <ian.brennan@icloud.com>
2015-06-16 11:38:30 -0600

    background and bootstrapped

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,28 +1,29 @@
-var keyDecoder = {'87':'w', '65':'a', '83':'s', '68':'d', '69':'e', '81':'q'};
+var decoder = {'keyDownDecoder' : {'87':'w', '65':'a', '83':'s', '68':'d', '69':'e', '81':'q'}, 'keyUpDecoder': {'65':'r', '68':'r'}}
 
 $(document).ready(function(){
+  function wait(keyCode, decodeObject){
+
+    console.log('post fired')
+    $.ajax({
+      type: "POST",
+      url: "/",
+      data: {"message": decoder[decodeObject][keyCode.toString()]},
+      success: function(data){
+        // console.log("received")
+      }
+    });
+
+  }
 
   $('body').on("keydown", function keyDown(e){
     var keyCode = e.keyCode;
-    if(keyDecoder.hasOwnProperty(keyCode)){
-      $.ajax({
-        type: "POST",
-        url: "/",
-        data: {"message": keyDecoder[keyCode.toString()]},
-        success: function(data){
-          console.log("received")
-        }
-      });
-    }else{
-      console.log(keyCode);
-    }
+    if(decoder['keyDownDecoder'].hasOwnProperty(keyCode)) setTimeout(function(){wait(keyCode, 'keyDownDecoder');}, 10)
+
   });
 
-  $('body').on('keyup', function keyUp(e){
-    var keyCode = e.keyCode;
-    if(keyCode){
-      console.log(keyCode)
-    }
-  })
+  // $('body').on('keyup', function keyUp(e){
+  //   var keyCode = e.keyCode;
+  //   if(decoder['keyUpDecoder'].hasOwnProperty(keyCode)) setTimeout(function(){wait(keyCode, 'keyUpDecoder');}, 10)
+  // })
 
 });
